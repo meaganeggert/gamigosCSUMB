01bcdb1392c11e02b1665b74b354c44e97398254
Meagan Eggert <meagan.eggert@gmail.com>
2025-11-12 11:21:36 -0800

    added a logout button on the drawer menu

--- app/src/main/java/com/example/gamigosjava/ui/activities/BaseActivity.java
+++ app/src/main/java/com/example/gamigosjava/ui/activities/BaseActivity.java
@@ -98,6 +98,15 @@ public abstract class BaseActivity extends AppCompatActivity {
                 overridePendingTransition(0,0);
                 finish();
                 return true;
+            } else if (id == R.id.nav_logout) {
+                // Sign Out of Firebase
+                FirebaseAuth.getInstance().signOut();
+
+                // Return to sign-in screen
+                Intent intent = new Intent(this, MainActivity.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+                finish();
             }
 
             return true;
